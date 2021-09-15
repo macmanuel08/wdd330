0347b0546a6af7664128742905b136b0625c57f6
John Mark Manuel <manueljm016@gmail.com>
2021-09-15 13:38:09 +0800

    portfolio page set up

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function recordContent() {
+    const contents = [
+        {
+            label: "Week 01 Notes",
+            url: "w1/index.html"
+        }
+    ];
+
+    for (let i = 0; i < contents.length; i++) {
+
+        const list = document.createElement('li');
+        const link = document.createElement('a');
+        link.innerHTML = contents[i].label;
+        link.setAttribute('href', contents[i].url);
+
+        list.appendChild(link);
+        document.querySelector('.directory').appendChild(list);
+    }
+}
+
+recordContent();
+
+});
