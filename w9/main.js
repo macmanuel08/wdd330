2312e1cc044ffc3c9d2735dbf7986bb4f326763c
John Mark Manuel <manueljm016@gmail.com>
2021-11-20 14:50:37 +0800

    Geolocation API

--- w9/main.js
+++ w9/main.js
@@ -1,36 +1,8 @@
-const URL = 'wss://echo.websocket.org/';
-const outputDiv = document.getElementById('output');
-const form = document.forms[0];
-const connection = new WebSocket(URL);
+console.log(navigator.geolocation)
 
-connection.addEventListener('open', () => {
-  output('CONNECTED');
-}, false);
+navigator.geolocation.getCurrentPosition(youAreHere);
 
-function output(message) {
-  const para = document.createElement('p');
-  para.innerHTML = message;
-  outputDiv.appendChild(para);
+function youAreHere(position) {
+  const locationPoint = document.getElementById("location");
+  locationPoint.innerHTML = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude} Timestamp: ${position.timestamp}`; 
 }
-
-function message(event) {
-  event.preventDefault();
-  const text = form.message.value;
-  output(`SENT: ${text}`);
-  connection.send(text);
-}
-
-form.addEventListener('submit', message, false);
-
-connection.addEventListener('message', (event) => {
-  output(`RESPONSE: ${event.data}`);
-  console.log(event.data); // even uses data property to access the returned data
-}, false);
-
-connection.addEventListener('close', () => {
-  output('DISCONNECTED'); // this event will occur when the connection is close
-}, false);
-
-connection.addEventListener('error', (event) => {
-output(`<span style='color: red;'>ERROR: ${event.data}</span>`); // Prints the error
-}, false);
